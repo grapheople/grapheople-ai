56929111fd541b2c5720a5c4cf707f8819f6204f
Pax <pax@pillyze.com>
2025-07-09 19:06:38 +0900

    add files

---
+++ grapheople-ai/src/main/java/com/grapheople/config/CustomConfig.java
@@ -0,0 +1,11 @@
+package com.grapheople.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+    "com.grapheople",
+})
+public class CustomConfig {
+}
---
+++ grapheople-ai/src/main/java/com/grapheople/controllers/ApiController.java
@@ -0,0 +1,21 @@
+package com.grapheople.controllers;
+
+import agents.multitool.MultiToolAgent;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Object> checkSensorDuplicated() {
+        MultiToolAgent.initAgent();
+        return ResponseEntity.ok("hello world");
+    }
+}
